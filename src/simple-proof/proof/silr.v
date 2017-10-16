ea0e3b6389edc68da85db31231a7267857b64905
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2017-10-16 06:58:06 +0200

    Remove commented out code

--- src/simple-proof/proof/silr.v
+++ src/simple-proof/proof/silr.v
@@ -28,7 +28,6 @@ Fixpoint tsize_flat_typ(T: typ) :=
     | typ_bot => 1
     | typ_all t1 t2 => S (tsize_flat_typ t1 + tsize_flat_typ t2)
     | typ_sel _ _ => 1
-    (* | typ_mem t1 t2 => S (tsize_flat_typ t1 + tsize_flat_typ t2)	 *)
     | typ_rcd d => tsize_flat_dec d
     | typ_bnd t => S (tsize_flat_typ t)
     | typ_and T1 T2 => S (tsize_flat_typ T1 + tsize_flat_typ T2)
@@ -39,7 +38,6 @@ with tsize_flat_dec(d : dec) :=
   | dec_trm m T   => S (tsize_flat_typ T)
   end.
 
-
 Lemma open_preserves_size_typ: forall T x j,
   tsize_flat_typ (open_rec_typ j x T) = tsize_flat_typ T
 with open_preserves_size_dec: forall d x j,
@@ -49,18 +47,9 @@ Proof.
   - induction d; intros; simpl; try reflexivity; erewrite ? open_preserves_size_dec; erewrite ? open_preserves_size_typ; try reflexivity.
 Qed.
 
-(** Reduction in an empty context *)
-Notation "t '|->' u" := (empty [t |-> u]) (at level 50).
-
-(* Definition red_n (n : nat) (t : trm) (v : val) := *)
-(*   match n with *)
-(*   | 0 -> t := *)
-(* Inductive red_n (n : nat) : (t : trm) (v : val) : Prop := *)
 Inductive red_n : nat -> sto -> trm -> val -> Prop :=
 | Red0 : forall e v, red_n 0 e (trm_val v) v
 | RedS : forall n e t1 t2 v, red e t1 t2 -> red_n n e t2 v -> red_n (S n) e t1 v.
-
-Require Import Binding.
 
 Definition nvsetFam := typ -> nvset.
 
@@ -109,27 +98,6 @@ Program Fixpoint val_type (T:typ) (k:nat) (v:val)
        False
   end.
 
-(* expr_type (T : typ) (n : nat) (t : trm) := False. *)
-
-
-(* Program Fixpoint val_type (env: list nvset) (GH:list nvset) (T:typ) (n:nat) (v:val) *)
-(*         {measure (existT (fun _ => nat) (tsize_flat_typ T) n) (termRel)}: Prop := *)
-(*   match v,T with *)
-(*     | val_lambda T0 t, typ_all T1 T2 => *)
-(*       lc_at_typ 0 T1 /\ lc_at_typ 1 T2 *)
-(*       (* /\ *) *)
-(*     (*   forall jj vx, *) *)
-(*     (*     (forall kx, (kx <= n -> val_type env GH T1 kx vx)) -> *) *)
-(*     (*     exists (jj2:nvset) v, t v /\ (forall k, val_type env (jj::GH) (open_typ (avar_f (length GH)) T2) k v) *) *)
-(*     | _, typ_and T1 T2 => *)
-(*       val_type env GH T1 n v /\ val_type env GH T2 n v *)
-(*     (* | _, TBind T1 => *) *)
-(*     (*   val_type *) *)
-(*     (*   closed 1 (length GH) (length env) T1 /\ *) *)
-(*     | _,_ => *)
-(*       False *)
-(*   end. *)
-
 Solve All Obligations.
 Show Obligation Tactic.
 Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omega. Qed.
@@ -139,34 +107,3 @@ Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omeg
 Ltac discriminatePlus := (split; intros; let Xf := fresh "Habs" in intro Xf; destruct Xf; discriminate).
 Obligation Tactic := discriminatePlus.
 Solve All Obligations.
-(* Next Obligation. split; intros; intro Habs; destruct Habs; discriminate. Qed. *)
-(* Next Obligation. split; intros; intro Habs; destruct Habs; discriminate. Qed. *)
-(* Next Obligation. split; intros; intro Habs; destruct Habs; discriminate. Qed. *)
-(* Next Obligation. *)
-
-(*                  apply left_lex. Tactics.program_simpl; try omega. Qed. *)
-(*                  try unfold open; try rewrite <-open_preserves_size; try omega. Qed. *)
-
-(* Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size; try omega. Qed. *)
-
-(* Next Obligation. unfold termRel; apply left_lex; Tactics.program_simpl. try unfold open; try rewrite <-open_preserves_size; omega. Qed. *)
-(* Next Obligation. unfold termRel. apply left_lex; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size; omega. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-(* (* Next Obligation. *) *)
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
-(* Next Obligation. split; Tactics.program_simpl; try unfold open; try rewrite <-open_preserves_size. Qed. *)
