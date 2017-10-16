0f0c70d3144dab6689ade04154a641d7384df25c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2017-10-16 06:59:00 +0200

    Parameterize val_type by stores, step up its induction proofs

--- src/simple-proof/proof/silr.v
+++ src/simple-proof/proof/silr.v
@@ -55,8 +55,12 @@ Definition pre_exp_type (val_type: nvsetFam)  (T:typ) (n:nat) (t : trm) := False
 
 (* Since the semantics *does* use a store (ahem, environment) this *should* be
  * parameterized by an environment as well. *)
-Program Fixpoint val_type (T:typ) (k:nat) (v:val)
+
+(* Beware: We just ignore type annotations in values, as usual in SILRs. *)
+
+Program Fixpoint val_type (env: sto) (T:typ) (k:nat) (v:val)
         {measure (existT (fun _ => nat) (tsize_flat_typ T) k) (termRel)}: Prop :=
+  lc_sto env /\
   (* let *)
   (*   exp_type (T : typ) (k : nat) (t: trm) := *)
   (*   forall v j, j < k -> *)
@@ -66,42 +70,27 @@ Program Fixpoint val_type (T:typ) (k:nat) (v:val)
   match v, T with
      | val_lambda T0 t, typ_all T1 T2 =>
        let
-         exp_type_t2 (k : nat) (t: trm) :=
+         exp_type_t2 x (env : sto) (k : nat) (t: trm) :=
          forall v j, j < k ->
-                red_n j empty t v ->
-                val_type T2 (k - j) v
+                red_n j env t v ->
+                val_type env (open_typ x T2) (k - j) v
        in
-       lc_at_typ 0 T1 /\ lc_at_typ 1 T2 /\
+       lc_at_typ (length env) T1 /\ lc_at_typ (S (length env)) T2 /\
        forall j va x,
          j < k ->
-         val_type T1 j va ->
+         val_type env T1 j va ->
          x \notin fv_trm t ->
-         (* This line is especially fishy: is this building an application to
-          * reduce, or reducing it already? Will this term take 1 step too much to
-          * reduce? It seems not, this just increases the depth of the
-          * derivation.
-          * Still, let's fix this with a proper definition. *)
-         exp_type_t2 j (trm_let (trm_val va) t)
-         (* exists v, red_n k empty (trm_let (trm_val va) t) v /\ *)
-         (* exists v, red_n k empty (trm_let (trm_val va) t) v /\ *)
-         (*      (forall k, val_type (open_typ x T2) k v) *)
-     (* exists (jj:nvset) v, t |-> v /\ *)
-     (*                   (forall k, exp_type (open_typ x T2) k v) *)
+         exp_type_t2 x (env & ((x ~ v))) j t
      | _, typ_and T1 T2 =>
-       val_type T1 k v /\ val_type T2 k v
-     (* | _, TBind T1 => *)
-     (*   val_type *)
-     (*   closed 1 (length GH) (length env) T1 /\ *)
+       val_type env T1 k v /\ val_type env T2 k v
      | _,_ =>
        False
   end.
 
-Solve All Obligations.
-Show Obligation Tactic.
-Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omega. Qed.
-Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omega. Qed.
-Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omega. Qed.
-Next Obligation. unfold termRel. apply left_lex. Tactics.program_simpl; try omega. Qed.
-Ltac discriminatePlus := (split; intros; let Xf := fresh "Habs" in intro Xf; destruct Xf; discriminate).
-Obligation Tactic := discriminatePlus.
-Solve All Obligations.
+Ltac smaller_calls :=
+  Tactics.program_simpl;
+  unfold termRel; apply left_lex;
+  try unfold open_typ; try rewrite open_preserves_size_typ; simpl; try omega.
+Ltac discriminatePlus := split; intros; let Habs := fresh "Habs" in intro Habs; destruct Habs; discriminate.
+Solve Obligations with smaller_calls.
+Solve Obligations with discriminatePlus.
